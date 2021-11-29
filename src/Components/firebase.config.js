a6a8117c9940f71b6e842965c7dc67277d3744b1
mashiourcse <mashiour.cseiiuc@gmail.com>
2021-11-29 18:53:16 +0600

    first commit

---
+++ src/Components/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNvovL-cjzmfkgNRUvN2hMCjvHh9NU6Pg",
+  authDomain: "smart-grocery-shop-ctg-94619.firebaseapp.com",
+  projectId: "smart-grocery-shop-ctg-94619",
+  storageBucket: "smart-grocery-shop-ctg-94619.appspot.com",
+  messagingSenderId: "287769391886",
+  appId: "1:287769391886:web:5a0ae1ab812820fb519d5f"
+};
+export default firebaseConfig;
